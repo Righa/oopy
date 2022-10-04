f8b44e8adf820b029c2299d1371de28f49077321
righa <lawrencierigha@gmail.com>
2022-10-04 19:22:26 +0300

    Add actual files

---
+++ app.py
@@ -0,0 +1,9 @@
+# OOP with python demo
+# from folder.file import obj
+
+from mclass.mclass import Mclass
+
+nclass = Mclass('Klass')
+
+nclass.do_first_thing()
+nclass.do_thing_with_str('Nstr')
---
+++ mclass/mclass.py
@@ -0,0 +1,13 @@
+
+class Mclass():
+
+	def __init__(self, name):
+		self.name = name
+
+
+	def do_first_thing(self):
+		print('first capability of your '+self.name)
+
+
+	def do_thing_with_str(self, str):
+		print('I can print your word - '+str)
